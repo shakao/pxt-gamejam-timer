62095459fd289b46a033d28998b690bbb732f985
Shannon Kao <shakao@microsoft.com>
2019-08-27 15:16:23 -0700

    Block to toggle debug looping of game

--- main.ts
+++ main.ts
@@ -4,9 +4,11 @@
 //% weight=100 color=#2ac9ac icon="\uf188"
 namespace gamejam {
     let _win: boolean;
+    let _debug: boolean;
 
     function init() {
         _win = false;
+        _debug = true;
         info.startCountdown(5);
         info.onCountdownEnd(end);
     }
@@ -26,7 +28,14 @@ namespace gamejam {
         _win = true;
     }
 
-    //% blockId=showtext block="show instruction"
+    //% blockId=setdebug block="set debug loop %on"
+    //% on.shadow=toggleonoff
+    //% weight=80
+    export function setDebug(on: boolean): void {
+        _debug = on;
+    }
+
+    //% blockId=showtext block="show instruction %s"
     export function showText(s: string): scene.Renderable {
         return scene.createRenderable(scene.HUD_Z, function (target: Image, camera: scene.Camera) {
             target.printCenter(s, 0);
@@ -44,7 +53,7 @@ namespace gamejam {
             }
             pause(750);
             r.destroy();
-            control.reset();
+            if (_debug) control.reset();
         })
     }
 
